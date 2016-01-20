35c5026d17096a11bde22f09c19f63f521386779
Taikakaulin <paukokkonen96@gmail.com>
2016-01-20 14:11:22 +0200

    Demo04

--- github/Demo04/Persomn/Person.cs
+++ github/Demo04/Persomn/Person.cs
@@ -8,6 +8,30 @@ namespace Persomn
 {
     class Person
     {
+        public string Firstname { get; set;  }
+        public string Lastname { get; set;  }
+        public string Address { get; set;  }
+        public int Age { get; set;  }
+        public string PhoneNumber { get; set;  }
 
+        public Person()
+        {
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            Firstname = firstName;
+            Lastname = lastName;
+        }
+
+        public void PersonMethod()
+        {
+            Console.WriteLine(" Person Does Something...");
+        }
+        public override string ToString()
+        {
+            return Firstname + " " + Lastname + " " + Address + " " + Age + " " + PhoneNumber;
+        }
     }
+
 }
---
+++ github/Demo04/T1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lift lift = new Lift();
+            while(true)
+            {
+                Console.WriteLine("The elevator is now in {0} floor: ", lift.Floor);
+                Console.WriteLine("Give a new floor number (1-5) > ");
+                lift.Floor = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
