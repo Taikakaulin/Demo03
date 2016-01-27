d9cd416114319c0310699b874281aa1d3465cf6d
Taikakaulin <paukokkonen96@gmail.com>
2016-01-27 14:26:19 +0200

    T1

---
+++ github/T1/T1/Persons.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    /// <summary>
+    /// This class holds persons objects and give a few methdos...
+    /// </summary>
+    class Persons
+    {
+        /// <summary>
+        /// persons holds person objects.
+        /// </summary>
+        private List<Person> persons;
+
+        /// <summary>
+        /// Default constructor intializes a collection
+        /// </summary>
+
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+        /// <summary>
+        /// Adds a person to collection
+        /// </summary>
+        /// <param name="person">Added persons</param>
+
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+        /// <summary>
+        /// Returns a person to caller application.
+        /// </summary>
+        /// <param name="index">Person index in collection</param>
+        /// <returns>Returned person object</returns>
+
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count) return persons.ElementAt(index);
+            else return null;
+
+        }
+
+        public Person FIndPerson(string socialSecurityNumber)
+        {
+            foreach(Person person in persons)
+            {
+                if (socialSecurityNumber.Equals(person.SocialSecurityNumber))
+                {
+                    // Person with sotu found..
+                    return person;
+                }
+            }
+            // Person with sotu not found..
+            return null;
+        }
+
+        public void PrintCollection()
+        {
+            Console.WriteLine("person in collection: ");
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
---
+++ github/T1/T1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an object from Persons
+            Persons myFriends = new Persons();
+
+            // create a friends...
+            Person person1 = new Person { Firstname = "Mikko", Lastname = "Mallikas", SocialSecurityNumber = "010101-010E" };
+            Person person2 = new Person { Firstname = "Henkka", Lastname = "Mäkinen", SocialSecurityNumber = "123456-111T" };
+            Person person3 = new Person { Firstname = "Laura", Lastname = "Joulunen", SocialSecurityNumber = "987456-456H" };
+
+
+            // add persons to collection
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+            // print collection
+            myFriends.PrintCollection();
+            
+
+            // get one person
+            Console.WriteLine("get one person from collection: ");
+            Person person4 = myFriends.GetPerson(1);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            } else
+            {
+                Console.WriteLine("Cannot find person in that position...");
+            }
+
+            // find person with sotu
+
+            string socialSecurityNumber = "123456-111T";
+            Console.WriteLine("Find person with sotu : " + socialSecurityNumber);
+            Person person5= myFriends.FIndPerson(socialSecurityNumber);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            } else
+            {
+                Console.WriteLine("Not found...");
+            }
+
+
+        }
+
+    }
+}
