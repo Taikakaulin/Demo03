5e5e42e8e0067ac6855a274f2bdf4727999eef2b
Taikakaulin <paukokkonen96@gmail.com>
2016-02-02 11:07:50 +0200

    Demo07

---
+++ github/demo07/TVApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shows> shows = new List<Shows>();
+            shows.Add(new Shows { Name = "The Simpsons", Channel = "Sub tv", Start = 20.30, End = 21.00, Info = "A very good show lads!" });
+            Stream WriteMultipleStream = new FileStream("TvShows.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+           IFormatte formatter.Serialize(WriteMultipleStream, shows);
+            WriteMultipleStream.Close();
+
+            Stream openstream = new FileStream("TvShows")
+        }
+    }
+}
---
+++ github/demo07/TVApp/Shows.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVApp
+{
+    class Shows
+    {
+        public string Name { get; set; }
+        public string Channel { get; set; }
+        public double Start { get; set; }
+        public double End { get; set; }
+        public string Info { get; set; }
+
+        public Shows()
+        { 
+        }
+
+        
+
+
+    }
+}
