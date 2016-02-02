ecae84f3c22382ff3dfcfccca0c6fe8ecbb52afc
Taikakaulin <paukokkonen96@gmail.com>
2016-02-02 10:43:41 +0200

    Demo07

---
+++ github/demo07/NumbApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Integers;
+            double Doubles;
+            string text;
+            System.IO.StreamWriter file = new System.IO.StreamWriter(@"T2Integers.txt");
+            System.IO.StreamWriter file2 = new System.IO.StreamWriter(@"T2Doubles.txt");
+            do
+            {
+                Console.Write("Give a number: ");
+                text = Console.ReadLine();
+                if (int.TryParse(text, out Integers))
+                {
+                    file.WriteLine(Integers);
+                }
+
+                else if(double.TryParse(text, out Doubles))
+                {
+                    file2.WriteLine(Doubles);
+                }
+
+
+
+            }
+            while (!string.IsNullOrEmpty(text));
+            file.Close();
+            file2.Close();
+
+            String[] lines = System.IO.File.ReadAllLines(@"T2Integers.txt");
+            Console.WriteLine("T2Integers.txt:\n");
+            foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            string[] lanes = System.IO.File.ReadAllLines(@"T2Doubles.txt");
+            Console.WriteLine("T2Doubles.txt:\n");
+            foreach(string lane in lanes)
+            {
+                Console.WriteLine(lane);
+
+            }
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ github/demo07/demo07/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "K";
+            System.IO.StreamWriter file = new System.IO.StreamWriter("Person.txt");
+            while(true)
+            {
+                Console.WriteLine("Write some text: ");
+                text = Console.ReadLine();
+                if(text == "0")
+                {
+                    break;
+                }
+                file.WriteLine(text);
+
+            }
+
+            file.Close();
+
+            try
+            {
+                string source = System.IO.File.ReadAllText("Person.txt");
+                System.Console.WriteLine(source);
+                Console.ReadLine();
+            } catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("File Not Found (FileNotFoundException");
+            }
+           
+
+        }
+    }
+}
