8fe1b11b37b71bb8f900dcbd08cdc6d93c22b78b
Taikakaulin <paukokkonen96@gmail.com>
2016-02-09 11:06:24 +0200

    Demo09

---
+++ github/Demo09/Car/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Car
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        int cars = 0;
+        int trucks = 0;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Cars_Click(object sender, RoutedEventArgs e)
+        {
+            cars++;
+            textBoxCars.Text = cars.ToString();
+
+        }
+
+        private void Trucks_Click(object sender, RoutedEventArgs e)
+        {
+            trucks++;
+            textBoxTrucks.Text = trucks.ToString();
+        }
+    }
+}
---
+++ github/Demo09/Markka/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Markka
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        
+        private string Calc(string raha, bool onkoeuro)
+        {
+            double vastaus;
+            double k;
+            if(double.TryParse(raha, out k))
+            {
+                if (onkoeuro)
+                {
+                    vastaus = k * 5.94573;
+
+                }
+                else
+                {
+                    vastaus = k * 0.168187;
+
+                }
+                return vastaus.ToString("0.00");
+
+            }
+            return " ";
+            
+          
+
+        }
+
+       
+
+        private void textBox1_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            textBox.Text = Calc(textBox1.Text, false);
+        }
+
+        private void textBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            textBox1.Text = Calc(textBox.Text, true);
+        }
+    }
+}
