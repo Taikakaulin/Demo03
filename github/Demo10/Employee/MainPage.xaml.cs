24ebb62cf8cc67639c491653cc080e4a2f630316
Taikakaulin <paukokkonen96@gmail.com>
2016-02-10 15:22:11 +0200

    Demo10

---
+++ github/Demo10/Employee/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Employee
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            // change default windows startup mode
+
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            //windows size
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("name is ", nameTextBox.Text);
+            Debug.WriteLine("Email Is ", EmailTextBox.Text);
+            Debug.WriteLine("Password is ", PasswordTextBox.Password);
+            //admin or user
+           if ((bool)NormalUserCheckbox.IsChecked)
+            {
+                Debug.WriteLine("Normal User!");
+            }
+           else
+            {
+                Debug.WriteLine("Admin User!");
+            }
+        }
+    }
+}
---
+++ github/Demo10/Shopping/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Shopping
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BuyingButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool) Milk.IsChecked)
+            {
+                List.Text += Milk.Content + " ";
+
+            }
+            if ((bool)Butter.IsChecked)
+            {
+                List.Text += Butter.Content + " ";
+            }
+            if ((bool)Beer.IsChecked)
+            {
+                List.Text += Beer.Content + " ";
+            }
+            if ((bool)Chicken.IsChecked)
+            {
+                List.Text += Chicken.Content + " ";
+            }
+            if ((bool)Lemonade.IsChecked)
+            {
+                List.Text += Lemonade.Content + " ";
+            }
+        }
+    }
+}
